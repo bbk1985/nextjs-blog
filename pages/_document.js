6afb5e435391ff2519d425c6edc636401896d25b
Ben Koh <ben@Bens-MacBook-Pro.local>
2022-11-30 19:41:56 +0800

    finished nextjs tutorial

---
+++ pages/_document.js
@@ -0,0 +1,13 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="cn">
+      <Head />
+      <body className="bg-yellow">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
